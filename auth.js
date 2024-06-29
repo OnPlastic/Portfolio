f07e1e4927527ba11824d3a4d11a39e99609acc2
OnPlastic <123543853+OnPlastic@users.noreply.github.com>
2024-06-30 00:21:31 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,7 +3,8 @@ document.addEventListener("DOMContentLoaded", async () => {
   const auth0 = await createAuth0Client({
     domain: 'dev-ui6ntlnkvcb0l503.eu.auth0.com',
     client_id: 'z2Ajp97sTYXnAzu8N4tAZc5heSbJnvrY',
-    redirect_uri: window.location.origin
+    redirect_uri: 'https://onplastic.github.io/Portfolio',
+    cacheLocation: 'localstorage'
   });
 
   const isAuthenticated = await auth0.isAuthenticated();
@@ -22,7 +23,8 @@ window.logout = async () => {
   const auth0 = await createAuth0Client({
     domain: 'dev-ui6ntlnkvcb0l503.eu.auth0.com',
     client_id: 'z2Ajp97sTYXnAzu8N4tAZc5heSbJnvrY',
-    redirect_uri: window.location.origin
+    redirect_uri: 'https://onplastic.github.io/Portfolio',
+    cacheLocation: 'localstorage'
   });
   auth0.logout({
     returnTo: window.location.origin
