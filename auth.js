93a59af4e4dcecc5fba2c2eafe6a5e806a5ee574
OnPlastic <123543853+OnPlastic@users.noreply.github.com>
2024-06-30 00:26:18 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,21 +1,24 @@
-// auth.js
 document.addEventListener("DOMContentLoaded", async () => {
   const auth0 = await createAuth0Client({
     domain: 'dev-ui6ntlnkvcb0l503.eu.auth0.com',
     client_id: 'z2Ajp97sTYXnAzu8N4tAZc5heSbJnvrY',
-    redirect_uri: 'https://onplastic.github.io/Portfolio',
+    redirect_uri: 'https://onplastic.github.io/Portfolio', // Stellen Sie sicher, dass dies exakt der konfigurierten URL entspricht
     cacheLocation: 'localstorage'
   });
 
+  const query = window.location.search;
+  if (query.includes('code=') && query.includes('state=')) {
+    await auth0.handleRedirectCallback();
+    window.history.replaceState({}, document.title, "/Portfolio");
+  }
+
   const isAuthenticated = await auth0.isAuthenticated();
 
-  if (!isAuthenticated) {
-    // Benutzer ist nicht authentifiziert, leite zur Auth0-Login-Seite weiter
-    await auth0.loginWithRedirect();
-  } else {
-    // Benutzer ist authentifiziert, erlaube Zugriff auf die Website
+  if (isAuthenticated) {
     const user = await auth0.getUser();
     console.log("Benutzerinfo:", user);
+  } else {
+    await auth0.loginWithRedirect();
   }
 });
 
@@ -23,10 +26,10 @@ window.logout = async () => {
   const auth0 = await createAuth0Client({
     domain: 'dev-ui6ntlnkvcb0l503.eu.auth0.com',
     client_id: 'z2Ajp97sTYXnAzu8N4tAZc5heSbJnvrY',
-    redirect_uri: 'https://onplastic.github.io/Portfolio',
+    redirect_uri: 'https://onplastic.github.io/Portfolio', // Stellen Sie sicher, dass dies exakt der konfigurierten URL entspricht
     cacheLocation: 'localstorage'
   });
   auth0.logout({
-    returnTo: window.location.origin
+    returnTo: 'https://onplastic.github.io/Portfolio' // Stellen Sie sicher, dass dies exakt der konfigurierten URL entspricht
   });
 };
